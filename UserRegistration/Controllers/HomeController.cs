4aad5e7d17a15723b0c0bba3b562c2db3494a026
Zellos <Zellos@WIN-UFO1CIKLBV0>
2024-07-31 13:14:42 +0900

    Add project files.

---
+++ UserRegistration/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Действия для регистрации пользователя
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.IsUserConfirm()) return RedirectToAction("Index", "Home");                
+                else return View(model);
+            }
+            else return View(model);
+        }
+
+        // Действие для отображения формы регистрации
+        [HttpGet]
+        public ActionResult Register()
+        {
+            RegisterModel model = new RegisterModel();
+            return View(model);
+        }        
+    }
+}
---
+++ UserRegistration/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace UserRegistration.Models
+{
+    //Класс модели
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        //Метод проверки на соответствие пароля
+        public bool IsUserConfirm()
+        {
+            if (!(UserName == ConfirmPassword))
+                return false;
+            return true;
+        }
+    }
+}
